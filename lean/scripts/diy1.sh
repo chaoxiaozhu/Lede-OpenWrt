44227070dfacc6509738019019f9a315dd6031a2
chao <82521372+chaoxiaozhu@users.noreply.github.com>
2021-05-10 16:45:25 +0800

    Update diy1.sh

--- lean/scripts/diy1.sh
+++ lean/scripts/diy1.sh
@@ -3,8 +3,9 @@
 #
 
 #添加自定义插件链接（自己想要什么就github里面搜索然后添加）
+git clone https://github.com/sirpdboy/sirpdboy-package package/sirpdboy
 git clone https://github.com/xiaorouji/openwrt-passwall package/xiaorouji
-git clone https://github.com/liuran001/openwrt-packages package/liuran001
+#git clone https://github.com/liuran001/openwrt-packages package/liuran001
 
 # 使用源码自带ShadowSocksR Plus+出国软件
 #sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
