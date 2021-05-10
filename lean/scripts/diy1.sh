227905e66e2c8fb28f735bd468fc208e58cbdfd3
chao <82521372+chaoxiaozhu@users.noreply.github.com>
2021-05-10 11:03:23 +0800

    Add files via upload

---
+++ lean/scripts/diy1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+
+#添加自定义插件链接（自己想要什么就github里面搜索然后添加）
+git clone https://github.com/xiaorouji/openwrt-passwall package/xiaorouji
+git clone https://github.com/liuran001/openwrt-packages package/liuran001
+
+# 使用源码自带ShadowSocksR Plus+出国软件
+#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
