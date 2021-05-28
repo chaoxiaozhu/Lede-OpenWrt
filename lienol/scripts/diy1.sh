b465727670df26f1e9d309db8994005630a481eb
chao <82521372+chaoxiaozhu@users.noreply.github.com>
2021-05-28 13:51:04 +0800

    Update diy1.sh

--- lienol/scripts/diy1.sh
+++ lienol/scripts/diy1.sh
@@ -3,7 +3,6 @@
 #
 
 # 添加自定义插件链接（自己想要什么就github里面搜索然后添加）
-#git clone https://github.com/chaoxiaozhu/packages.git package/chao
 git clone https://github.com/xiaorouji/openwrt-passwall package/xiaorouji
 git clone https://github.com/liuran001/openwrt-packages package/liuran001
 
